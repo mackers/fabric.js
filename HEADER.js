1709ef9d22c50497998c561fb53d9ba7e8eefadd
David McNamara <david@rocksteady.com>
2014-02-20 16:27:34 +0000

    fixed exports munging

--- HEADER.js
+++ HEADER.js
@@ -1,6 +1,9 @@
 /*! Fabric.js Copyright 2008-2013, Printio (Juriy Zaytsev, Maxim Chernyak) */
 
-var exports = exports || {};
+if (typeof exports !== 'undefined') {
+    exports.fabric = fabric;
+}
+
 var fabric = fabric || { version: "1.3.0" };
 
 // make sure exports.fabric is always defined when used as 'global' later scopes
